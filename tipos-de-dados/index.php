a457160d8126c0ca70a00bf5d1c02687855e4eb0
alexandretoscano23 <toscanodinizalexandre32@gmail.com>
2024-10-03 15:44:22 -0300

    arrays

--- tipos-de-dados/index.php
+++ tipos-de-dados/index.php
@@ -6,6 +6,9 @@
     <title>Estudando Tipos de Dados</title>
 </head>
 <body>
+    <h1>Estudando Tipos de Dados</h1>
+    <hr>
+    <br>
     <?php
 
         echo 5; //int - inteiro
@@ -22,6 +25,14 @@
         echo "<br>";
         echo true; //boolean - true ou false  
 
+        //verificar qual o tipo de dado
+
+        $inteiro = 5;
+        $flutuante = 5.5;
+        $boole = true;
+        $str = "Olá Mundo";
+
+        echo"$inteiro é do tipo " . gettype($inteiro); //gettype() é uma função que retorna o tipo do dado selecionado.
     ?>
 </body>
 </html>
