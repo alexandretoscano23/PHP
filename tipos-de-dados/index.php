2fe75394a9f6bbd4ea674ed7d84c9f2d652d03d4
alexandretoscano23 <toscanodinizalexandre32@gmail.com>
2024-10-03 10:41:37 -0300

    tipos de dados

---
+++ tipos-de-dados/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudando Tipos de Dados</title>
+</head>
+<body>
+    <?php
+
+        echo 5; //int - inteiro
+        echo "<br>"; //quebra de linha
+        echo -5; //int - inteiro
+        echo "<br>"; //quebra de linha
+        echo 5.5; //float - ponto flutuante
+        echo "<br>";
+        echo -3.2; //float - ponto flutuante
+        echo "<br>"; 
+        echo "Olá Mundo"; //string - (dentro de "")
+        echo "<br>";
+        echo "5"; //string - (dentro de "")
+        echo "<br>";
+        echo true; //boolean - true ou false  
+
+    ?>
+</body>
+</html>
