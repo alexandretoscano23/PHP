9ed0a668c130b1f55cfcbd81be5b06246bb9c18b
alexandretoscano23 <toscanodinizalexandre32@gmail.com>
2024-10-17 09:27:19 -0300

    orientacao a objetos

---
+++ orientacao-a-objetos/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Orientação a Objetos</title>
+</head>
+<body>
+    <h1>Estudando Orientação a objetos</h1>
+    <hr>
+    <br>
+
+
+    
+    ?>
+</body>
+</html>
