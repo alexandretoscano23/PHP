83f343be3ed82859ec5ec02952e0c0eaee41e972
alexandretoscano23 <toscanodinizalexandre32@gmail.com>
2024-10-17 22:02:25 -0300

    public,private,static

--- orientacao-a-objetos/private-public-static/exemplo.class.php
+++ orientacao-a-objetos/private-public-static/exemplo.class.php
@@ -1,16 +1,30 @@
 <?php
+//arquivo para conter a class de exemplo
 
-    //public funciona em qualquer lugar
-    //private so pode ser acessada dentro da class
-    class ex{
-    private $var1;
-    public $var2;
+    //public funciona em qualquer lugar,private so conseguimos acessar dentro da class
+    class exemple{
 
-    public function metodo(){
+        private $name;
+        public $name2;
 
+        public static $name3 = 'estático';
+
+        public static function user(){
+            echo 'Função estática';
+        }
+
+        public function varPrivate($nameUser){ //cria uma função para passar um valor para a variável privada "name"
+            $this -> name = $nameUser; //o name ao lado do this é para referenciar a variável name da class. A segunda foi criada como parâmetro da função
+
+            //this "este" - referência algo
+            //this name = $name2 -> seria como : "este nome é igual a nome2"
+        }
+
+        public function getName(){
+            return $this -> name;
+        }
     }
-    }
 
-
+    
 
 ?>
--- orientacao-a-objetos/private-public-static/index.php
+++ orientacao-a-objetos/private-public-static/index.php
@@ -7,17 +7,49 @@
 </head>
 <body>
     <?php
-    include('exemplo.class.php'); //incluindo outro arquivo php
+
+    include('exemplo.class.php');
 
     //instância de objeto
-    $ex = new ex();
-    
-    $ex -> var1 = 'Olá';//erro pois é privada e não pode ser acessada
-    $ex -> var2 = 'Olá';//irá mostrar normalmente
+    $exemple = new exemple();
 
-    echo $ex -> var2;
+    //$exemple -> name = 'Olá'; //erro pois é privada, apenas na class que pode acessar
+
+    $exemple -> name2 = 'Olá';
+
+    echo $exemple -> name2;
     
+    echo '<br>';
+
+    $exemple2 = new exemple();
+    $exemple2 -> name2 = 'Tchau'; 
+
+    echo $exemple2 -> name2; 
+    $exemple2 -> name2;
    
+    //cada variável tem o mesmo objeto mas são instâncias diferentes. Mesmo nome mas os valores são diferentes
+
+    echo '<br>';
+
+    echo exemple::$name3;//para acessar uma variável estática, usa o :: ao invés de ->. Não pode atribuir valores individuais. Vai ser sempre uma coisa só. Sem criar variáveis.
+
+    echo '<br>';
+    
+    echo exemple::user();//acessar a fuinção estática
+
+    echo '<br>';
+
+    $exemple -> varPrivate('Alexandre Toscano Diniz'); //passa o valor para o parâmetro da função criada
+    echo $exemple -> getName();
+
+    //criando um objeto com o mesmo nome e instâncias diferentes. - com function
+
+    echo '<br>';
+
+    $exemple2 -> varPrivate('Pedro Gomes Alves');
+    echo $exemple2 -> getName();
+
+    //mesmo tendo o mesmo nome, suas instâncias são diferentes. Cada um tem seu valor
     ?>
 </body>
 </html>
